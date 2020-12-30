c555eabc75b8198f54d87411ac705a237681ac7a
jdhofmann <justindhofmann@gmail.com>
2020-12-30 13:54:04 -0500

    my solution, in 15 minutes

--- script.js
+++ script.js
@@ -135,17 +135,20 @@ function CodelandUsernameValidation(str) {
 
 // Have the function FindIntersection(strArr) read the array of strings stored in strArr which will contain 2 elements: the first element will represent a list of comma-separated numbers sorted in ascending order, the second element will represent a second list of comma-separated numbers (also sorted). Your goal is to return a comma-separated string containing the numbers that occur in elements of strArr in sorted order. If there is no intersection, return the string false.
 // Examples
-// Input: ["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"]
+let input = ["1, 3, 4, 7, 13", "1, 2, 4, 7, 8, 13, 15"]
 // Output: 1,4,13
-// Input: ["1, 3, 9, 10, 17, 18", "1, 4, 9, 10"]
+// let input = ["1, 3, 9, 10, 17, 18", "1, 4, 9, 10"]
 // Output: 1,9,10
 
 function FindIntersection(strArr) { 
-
-  // code goes here  
-  return strArr; 
-
+  let one = strArr[0].split(', ')
+  let two = strArr[1].split(', ')
+  let intersection = [];
+  for(let i = 0; i<one.length; i++){
+    if(two.includes(one[i])){
+      intersection.push(one[i])
+    }
+  }
+  return intersection; 
 }
-   
-// keep this function call here 
-console.log(FindIntersection(readline()));
+console.log(FindIntersection(input));
