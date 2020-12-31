891c0fb574b8af9c3a6bf5e35607e9d3c817b6b6
jdhofmann <justindhofmann@gmail.com>
2020-12-31 14:26:40 -0500

    new problem added

--- script.js
+++ script.js
@@ -174,9 +174,9 @@ function FindIntersection(strArr) {
 // Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. For example: if the input string is "Hello World and Coders" then your program should return the string sredoC dna dlroW olleH.
 
 // Example
-let one = "coderbyte"
+// let one = "coderbyte"
 // Output: etybredoc
-let two = "I Love Code"
+// let two = "I Love Code"
 // Output: edoC evoL I
 
 function FirstReverse(str) { 
@@ -195,8 +195,29 @@ function FirstReverse(str) {
   // return result
 
 
-  
+
 
 }
 
-console.log(FirstReverse(two));
+// console.log(FirstReverse(two));
+
+
+// First Factorial
+
+// Have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it. For example: if num = 4, then your program should return (4 * 3 * 2 * 1) = 24. For the test cases, the range will be between 1 and 18 and the input will always be an integer.
+// Examples
+
+let one = 4
+// Output: 24
+let two = 8
+// Output: 40320
+
+function FirstFactorial(num) { 
+
+  // code goes here  
+  return result; 
+
+}
+   
+// keep this function call here 
+console.log(FirstFactorial(one));
